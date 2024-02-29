23cc8a80405c7e06db93a2a52a5db1e47e325559
Frank742129 <78746023+Frank742129@users.noreply.github.com>
2024-02-29 14:28:20 +0100

    Update widget.js

--- apps/widbt/widget.js
+++ apps/widbt/widget.js
@@ -15,3 +15,33 @@ WIDGETS["bluetooth"]={area:"tr",width:15,draw:function() {
 }};
 NRF.on('connect',WIDGETS["bluetooth"].changed);
 NRF.on('disconnect',WIDGETS["bluetooth"].changed);
+/*
+** Advertise a writeable characteristic. Accepts text (in 20 char
+** chunks) terminated with __EOM__ by itself. If there's text, show
+** it (as an alarm), otherwise reload the alarm & msg files (empty
+** string signals another BLE process updated those files)
+*/
+var BLEMessage = "";
+NRF.setServices({
+  "0x190a": {
+    "0x0002": {
+      value : [0],
+      maxLen : 20,
+      writable : true,
+      onWrite : function(evt) {
+        let str = stringFromArray(evt.data);
+        if(str === "__EOM__") {
+          if(BLEMessage) {
+            /* showMsg('Message',BLEMessage);*/
+          }
+          BLEMessage = '';
+        } else {
+          BLEMessage += str;
+        }
+      }
+    }
+  }
+}, { });
+
+// Change name to 'Flag'
+NRF.setAdvertising({}, {name:"Golfwatch"});
